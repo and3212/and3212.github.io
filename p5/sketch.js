da013f0f17d4934615028d8c4b954216f00e9d27
and3212 <and3212@gmail.com>
2018-05-13 01:32:43 -0400

    Made it draw every 3 dots and made the tolerance smaller

--- p5/sketch.js
+++ p5/sketch.js
@@ -9,7 +9,7 @@ var x1;
 var y1;
 var x2;
 var y2;
-var MOTORSTEP = 0.25 * Math.PI / 180.0;
+var MOTORSTEP = 0.05 * Math.PI / 180.0;
 var addPi1;
 var addPi2;
 
@@ -55,69 +55,71 @@ function draw() {
     translate(60, (60 + (l1+l2)/2) -height);
     translate(l1 + l2, 0);  // Move the origin to the center of the arc
   
-    //Math for angles
-    switch (command[i]) {
-        case "0x1":
-            theta1 += MOTORSTEP;
-            break;
-        case "0x3":
-            theta1 -= MOTORSTEP;
-            break;
-        case "0x4":
-            theta2 += MOTORSTEP;
-            break;
-        case "0xC":
-            theta2 -= MOTORSTEP;
-            break;
+	for (var k = 0; k < 3; k++) {
+		//Math for angles
+		switch (command[i]) {
+		    case "0x1":
+		        theta1 += MOTORSTEP;
+		        break;
+		    case "0x3":
+		        theta1 -= MOTORSTEP;
+		        break;
+		    case "0x4":
+		        theta2 += MOTORSTEP;
+		        break;
+		    case "0xC":
+		        theta2 -= MOTORSTEP;
+		        break;
 
-        case "0x5":
-            theta1 += MOTORSTEP;
-            theta2 += MOTORSTEP;
-            break;
-        case "0x7":
-            theta1 -= MOTORSTEP;
-            theta2 += MOTORSTEP;
-        case "0xD":
-            theta1 += MOTORSTEP;
-            theta2 -= MOTORSTEP;
-        case "0xF":
-            theta1 -= MOTORSTEP;
-            theta2 -= MOTORSTEP;
+		    case "0x5":
+		        theta1 += MOTORSTEP;
+		        theta2 += MOTORSTEP;
+		        break;
+		    case "0x7":
+		        theta1 -= MOTORSTEP;
+		        theta2 += MOTORSTEP;
+		    case "0xD":
+		        theta1 += MOTORSTEP;
+		        theta2 -= MOTORSTEP;
+		    case "0xF":
+		        theta1 -= MOTORSTEP;
+		        theta2 -= MOTORSTEP;
 
-        case "0xA":
-            laserEnabled = true;
-            break;
-        case "0xB":
-            laserEnabled = false;
-            break;
-        case "0xE":
-            i++;
-            if (i > command.size - 1) {
-                remove();
-			}
-            
-            laserIntensity = unhex(command[i].substring(2, command[i].length));
-			break;
-    }
+		    case "0xA":
+		        laserEnabled = true;
+		        break;
+		    case "0xB":
+		        laserEnabled = false;
+		        break;
+		    case "0xE":
+		        i++;
+		        if (i > command.size - 1) {
+		            remove();
+				}
+		        
+		        laserIntensity = unhex(command[i].substring(2, command[i].length));
+				break;
+		}
 
-    px2 = (int)(cos(theta1) * l1);
-    py2 = (int)(sin(theta1) * l1);
-    px3 = (int)(cos(theta2 + theta1) * l2) + px2;
-    py3 = (int)(sin(theta2 + theta1) * l2) + py2;
-
-
-    strokeWeight(2);
-    if (laserEnabled) {
-        stroke(0, 100, 50, laserIntensity);
-    } else {
-        stroke(100, 50, 0, 100);
-    }
-
-    point(px3, py3);
+		px2 = (int)(cos(theta1) * l1);
+		py2 = (int)(sin(theta1) * l1);
+		px3 = (int)(cos(theta2 + theta1) * l2) + px2;
+		py3 = (int)(sin(theta2 + theta1) * l2) + py2;
 
 
-    i++;
-    if (i > command.size-1) {
-		i = 0;
-    }
+		strokeWeight(2);
+		if (laserEnabled) {
+		    stroke(0, 100, 50, laserIntensity);
+		} else {
+		    stroke(100, 50, 0, 100);
+		}
+
+		point(px3, py3);
+
+
+		i++;
+		if (i > command.size-1) {
+			i = 0;
+		}
+	}
 }
